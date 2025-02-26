e2dd7e8b7e45744568973d587825dbef3302519f
Haroon Adeel <haroonadeel69@gmail.com>
2025-02-26 17:02:19 +0500

    Add files via upload

---
+++ 068-3A-Lab5/Lab.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	string data;
+	Node *next;
+	Node(string data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+class linkedList
+{
+public:
+	Node *head;
+
+	linkedList()
+	{
+		head = nullptr;
+	}
+
+	void insertFront(string data)
+	{
+		Node *newNode = new Node(data);
+		newNode->next = head;
+		head = newNode;
+	}
+
+	void insertNth(int position, string data)
+	{
+		Node *newNode = new Node(data);
+
+		if (position == 1)
+		{
+			newNode->next = head;
+			head = newNode;
+			return;
+		}
+
+		Node *temp = head;
+		for (int i = 1; i < position - 1 && temp != nullptr; i++)
+		{
+			temp = temp->next;
+		}
+		if (temp == nullptr)
+		{
+			cout << "Invalid position!\n";
+			return;
+		}
+
+		newNode->next = temp->next;
+		temp->next = newNode;
+	}
+
+	void insertLast(string data)
+	{
+		Node *newNode = new Node(data);
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			return;
+		}
+
+		Node *temp = head;
+		while (temp->next != nullptr)
+		{
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+
+	void printList()
+	{
+		Node *temp = head;
+		while (temp != nullptr)
+		{
+			cout << temp->data << " -> ";
+			temp = temp->next;
+		}
+		cout << "nullptr" << endl;
+	}
+
+};
+
+int main()
+{
+	linkedList list;
+	list.insertFront("Haroon ");
+	list.insertFront("David ");
+	list.insertFront("Mubeen ");
+	list.insertLast("Last ");
+	cout << "Printing Linked List: ";
+	list.printList();
+
+	return 0;
+}
